b102b8b1a4a9673450025f3ac25dedbfa1c5429d
Josh Homann <jhomann@isbx.com>
2018-02-19 01:05:26 +0200

    add strings with pin attachment

--- DynamicAnimatorAttachment/ViewController.swift
+++ DynamicAnimatorAttachment/ViewController.swift
@@ -12,24 +12,45 @@ import CoreMotion
 class ViewController: UIViewController {
     // MARK: - Variables
     @IBOutlet var stars: [UIImageView]!
+    private var strings: [UIView] = []
     private var animator: UIDynamicAnimator!
     private var gravity: UIGravityBehavior!
     private var pushBehavior: UIPushBehavior!
     // MARK: - Constants
     let motionManager = CMMotionManager()
+    let stringLengths: [CGFloat] = [75, 100, 150, 120, 80]
     // MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        let attachements = stars.map { (star: UIImageView) ->  UIAttachmentBehavior in
-            let anchor = star.center
-            let behavior = UIAttachmentBehavior(item: star, attachedToAnchor: anchor)
-            behavior.length = 150
-            behavior.frictionTorque = 10
-            return behavior
+        strings = zip(stars, stringLengths).map { (star,stringLength) -> UIView in
+            let string = UIView()
+            string.translatesAutoresizingMaskIntoConstraints = false
+            self.view.insertSubview(string, at: 0)
+            string.backgroundColor = .lightGray
+            string.widthAnchor.constraint(equalToConstant: 3).isActive = true
+            string.heightAnchor.constraint(equalToConstant: stringLength).isActive = true
+            string.centerXAnchor.constraint(equalTo: star.centerXAnchor).isActive = true
+            string.bottomAnchor.constraint(equalTo: star.centerYAnchor).isActive = true
+            return string
         }
         animator = UIDynamicAnimator(referenceView: view)
-        attachements.forEach { self.animator.addBehavior($0) }
-        gravity = UIGravityBehavior(items: stars)
+        view.layoutIfNeeded()
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        var attachments: [UIDynamicBehavior] = [UIAttachmentBehavior(item: view, attachedToAnchor: view.center)]
+        zip(stars, strings).forEach { (star, string) in
+            let anchor = CGPoint(x: string.center.x, y: string.frame.origin.y)
+            let stringAttachment = UIAttachmentBehavior(item: string, offsetFromCenter: UIOffset(horizontal: 0, vertical: -string.frame.size.height / 2 - star.frame.size.height / 2), attachedToAnchor: anchor)
+            let starAttachment =  UIAttachmentBehavior.pinAttachment(with: star, attachedTo: string, attachmentAnchor: star.center)
+            starAttachment.frictionTorque = 100
+            attachments.append(stringAttachment)
+            attachments.append(starAttachment)
+        }
+        animator.removeAllBehaviors()
+        attachments.forEach { self.animator.addBehavior($0) }
+        gravity = UIGravityBehavior(items: [stars,strings].flatMap{$0})
         animator.addBehavior(gravity)
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tap))
         view.addGestureRecognizer(tapGesture)
@@ -45,14 +66,10 @@ class ViewController: UIViewController {
                 }
                 self.pushBehavior = UIPushBehavior(items: [star], mode: .instantaneous)
                 self.pushBehavior.pushDirection = direction
-                self.pushBehavior.magnitude = 0.2
+                self.pushBehavior.magnitude = 0.1
                 self.animator.addBehavior(self.pushBehavior)
             }
         }
     }
-    
-
-
-
 }
 
